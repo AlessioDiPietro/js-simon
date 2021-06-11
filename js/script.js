c48dec8d22efeacef94557cb70155f3eda8e8bbe
AlessioDPT <alessio.recos@gmail.com>
2021-06-11 15:45:03 +0200

    ADD- ALLERT NUMERI RANDOM

--- js/script.js
+++ js/script.js
@@ -2,3 +2,17 @@
 // Da li parte un timer di 30 secondi.
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+// Un alert() espone 5 numeri generati casualmente.
+document.getElementById("avvio").addEventListener("click", function(){
+
+    var simonDice = [];
+    
+    for (i=0;i<5;i++){
+        var numeroRand = Math.floor(Math.random()*99)+1;
+        simonDice.push(numeroRand);
+    }
+    alert(simonDice);
+
+})
